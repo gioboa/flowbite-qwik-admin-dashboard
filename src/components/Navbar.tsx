f9b9d61f9a49e029df72415ceb09eabd6701cd8c
gioboa <giorgiob.boa@gmail.com>
2024-01-15 21:27:00 +0100

    feat: add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { component$, useSignal } from "@builder.io/qwik";
+import { MoonIcon } from "./icons/MoonIcon";
+import { QwikIcon } from "./icons/QwikIcon";
+import { SunIcon } from "./icons/SunIcon";
+
+export const Navbar = component$(() => {
+  const themeSig = useSignal<"DARK" | "LIGHT">("LIGHT");
+
+  return (
+    <nav class="fixed z-30 w-full border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800">
+      <div class="mx-auto flex flex-wrap items-center justify-between">
+        <div class="w-full p-3 lg:px-5 lg:pl-3">
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <a href="/" class="flex items-center">
+                <QwikIcon theme={themeSig.value} />
+              </a>
+            </div>
+            <div class="flex items-center gap-3">
+              <button
+                class="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                data-testid="dark-theme-toggle"
+                type="button"
+                aria-label="Toggle dark mode"
+                onClick$={() => {
+                  themeSig.value = themeSig.value === "DARK" ? "LIGHT" : "DARK";
+                  document.body.classList.toggle("dark");
+                }}
+              >
+                {themeSig.value === "DARK" ? <SunIcon /> : <MoonIcon />}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+});
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,6 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
+import { Navbar } from "~/components/Navbar";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -13,5 +14,12 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
 };
 
 export default component$(() => {
-  return <Slot />;
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Slot />
+      </main>
+    </>
+  );
 });
